8c9f343f1548fec7a6fc625a4db1a1f85b5b9d92
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-09-15 16:46:29 +0200

    Update moving-0.py

--- moving-0.py
+++ moving-0.py
@@ -1,1 +1,10 @@
 # File that manages connection with robot using bluedot and gpiozero, and allows to control manually the movement 
+
+from bluedot import *
+from gpiozero import *
+from signal import pause
+
+# Servo pin attribution, what interface to use ? 
+
+
+
