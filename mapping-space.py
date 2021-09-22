bd8ed8cfbf464a7f52bc16a34a5e915e6a5d97aa
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-09-22 16:53:48 +0200

    Update mapping-space.py

--- mapping-space.py
+++ mapping-space.py
@@ -1,1 +1,11 @@
 # Python script to map scpace around
+"""
+Algorithm explanation :
+
+- Rotate the distance sensor
+- scan at every moment the distance
+  --> states the position in relation to the tags
+  --> states the distance
+  -> stores informations in a file (or in a matrix)
+
+"""
