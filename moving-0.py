799bca110422ebdc89e27895b5b4e00e53b0b20b
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-09-22 16:38:54 +0200

    Update moving-0.py

--- moving-0.py
+++ moving-0.py
@@ -3,7 +3,13 @@
 from bluedot import *
 from gpiozero import *
 from signal import pause
+from time import *
+from adafruit_servokit import * # only using ServoKit ?
 
-# Servo pin attribution, what interface to use ? -----> I2C interface, look with Adafruit
 
-
+kit = ServoKit(channels=16)
+# funcs : kit.servo[i].angle = n 
+#         kit.servo[i].actuation_range = n
+#         kit.servo[i].set_pulse_widht_range(min = 1000, max = 2000)
+#         kit.continuous_servo[i].throttle = j (max 1, min 0)
+# i is the channel the servo is connected to
