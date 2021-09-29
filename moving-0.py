f20eb681ce5b27756e2432aa5c0ad6c0f85aa159
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-09-29 16:31:38 +0200

    Update moving-0.py

--- moving-0.py
+++ moving-0.py
@@ -1,4 +1,5 @@
 # File that manages connection with robot using bluedot and gpiozero, and allows to control manually the movement 
+# This is only an API for facilitating communication with servos and motors
 
 from bluedot import *
 from gpiozero import *
@@ -13,3 +14,20 @@ kit = ServoKit(channels=16)
 #         kit.servo[i].set_pulse_widht_range(min = 1000, max = 2000)
 #         kit.continuous_servo[i].throttle = j (max 1, min 0)
 # i is the channel the servo is connected to
+ 
+"""
+Config : 
+
+1 and 2 : Main continuous servo
+
+**************
+Mode of operation :
+there are mutiple choices, but this is very important for the following functionnement of the bot...
+
+takes as argument a bool : --> uses while, good if the motor can be free-running (no need to stop and re-start before each loop...)
+
+takes as argument a timeout : will very probably function (no physical jitter) but will be less acccurate on moves, and can't stop moving at each moment
+
+
+
+"""
