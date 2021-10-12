fe57a3e60c926f983975a7087360d1f5e951618f
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-10-12 13:02:06 +0200

    Update moves.py

--- py-scripts/moves.py
+++ py-scripts/moves.py
@@ -7,8 +7,8 @@ from signal import pause
 from time import *
 from adafruit_servokit import * # only using ServoKit ?
 
-
 kit = ServoKit(channels=16)
+
 # funcs : kit.servo[i].angle = n 
 #         kit.servo[i].actuation_range = n
 #         kit.servo[i].set_pulse_widht_range(min = 1000, max = 2000)
@@ -19,18 +19,17 @@ kit = ServoKit(channels=16)
 Config : 
 
 1 and 2 : Main continuous servo
+3 and 4 : optional continuous servos
 
-**************
-Mode of operation :
-there are mutiple choices, but this is very important for the following functionnement of the bot...
-
-takes as argument a bool : --> uses while, good if the motor can be free-running (no need to stop and re-start before each loop...)
-
-takes as argument a timeout : will very probably function (no physical jitter) but will be less acccurate on moves, and can't stop moving at each moment
-
-
-
+5 : main head-rotating servo (180°... --> precise, not heavy / powerful)
 """
 
-def forward(boolF):
-    pass
+def forward(boolFunc, speed=1):
+	kit.continuous_servo[1].throttle = speed
+	kit.continuous_servo[2].throttle = -speed
+	# starting here a new thread would be nice...
+	# then call a function that close both servos
+	while boolFunc():
+		pass
+	kit.continuous_servo[1].throttle = kit.continuous_servo[2].throttle = 0
+	
