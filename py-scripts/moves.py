7a306e0d9cf277e42219759c88812903dfd32b9e
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-10-13 17:12:28 +0200

    Update moves.py

--- py-scripts/moves.py
+++ py-scripts/moves.py
@@ -10,23 +10,27 @@ import threading
 from adafruit_servokit import * # only using ServoKit ?
 
 # Other custom localisation scripts are to be imported ...
-
-kit = ServoKit(channels=16)
-
 # funcs : kit.servo[i].angle = n 
 #         kit.servo[i].actuation_range = n
 #         kit.servo[i].set_pulse_widht_range(min = 1000, max = 2000)
 #         kit.continuous_servo[i].throttle = j (max 1, min 0)
 # i is the channel the servo is connected to
  
+#------------------ CONSTANTS  ------------------------
+
+l = 4.5 # corresponds to width/2 of vehicle
+L = 15 # corresponds to length of vehicle
+kit = ServoKit(channels=16) 
 """
 Config : 
 
 1 and 2 : Main continuous servo
 3 and 4 : optional continuous servos
 
-5 : main head-rotating servo (180°... --> precise, not heavy / powerful)
+5 : main head-rotating servo (180°... --> precise, not heavy / powerful : --> tryong feedback servo would be great)
 """
+
+#------------------  MAIN CUSTOM CLASSES  ---------------------
 
 class boolMoveThread(threading.Thread) :
 	# This type of thread is to be created within a forward or backward function, and it lets run the main motors until a condition
@@ -43,11 +47,14 @@ class boolMoveThread(threading.Thread) :
 			pass
 		kit.continuous_servo[1].throttle = kit.continuous_servo[2]
 
+#--------------  MAIN FUNCS  -----------------
+		
 def forward(boolFunc, speed=1):
-	# starting here a new thread would be nice...
-	# then call a function that close both servos
+	
+	# runs the main motors at speed between 0 and 1
 	thread1 = boolMoveThread(boolFunc, speed)
 	thread1.start()
+	# calling .join() for waiting ?
 	
-	
+
 	
