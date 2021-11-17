81d85afe6b2f44054b18409cb683352c4be4a859
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-11-17 17:20:48 +0100

    Add files via upload

---
+++ global_funcs.py
@@ -0,0 +1,10 @@
+# Script that will contain the necessary consts and functions 
+
+L = 10.5
+l = 4.5
+
+def rotationToPoint(X, Y, theta, x, y):
+	# function that calculates the angle of rotation to go from (X, Y) to (x, y) with theta initial angle
+	pass
+
+# etc...
