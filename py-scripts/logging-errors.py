b61a93a747c973a8a4a325e7e2f0ceaa4a41ec32
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-12-01 17:20:29 +0100

    Delete logging-errors.py

--- py-scripts/logging-errors.py
+++
@@ -1,4 +0,0 @@
-# This script is intended to report errors that occure during the functionement and to report them on
-# a communication canal (example SSh, LED matrix etc...) 
-
-# link to main launcher script ?
