cb6c17828ccd1d9721fa2348db0c7463ae5f7dae
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2022-03-25 10:09:09 +0100

    Update localisation.py

--- py-scripts/localisation.py
+++ py-scripts/localisation.py
@@ -1,34 +1,49 @@
-# Python script to get localisation in space around
+# Python script to get localisation in space around + get points around
 
-########### SENSORS INPUTS MANAGEMENT ############
 
-# public data : estimatePosition() : returns an estimation of the state when called
-# estimateSpeed()
-# estimateRotation() (of head)
+#  SENSORS INPUTS MANAGEMENT #
 
-from gpiozero import DistanceSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
+
 from mpu6050 import mpu6050
 from global_funcs import *
+import numpy as np
+import time
 
-# HC-SR04, distance sensor
-
-X, Y = 0, 0
-
-myFactory = PiGPIOFactory()
-distanceSensor = DistanceSensor(max_range=4, pin_factory=myFactory, echo=..., trig=...)
 
 # accel_sensor mpu6050
-accelSensor = mpu6050(0x68)
-
-def getAccelerationData() : 
-    return accelSensor.get_accel_data()
-
-def getGyroData() : 
-    return accelSensor.get_gyro_data()
+accelSensor = mpu6050(0x68)  # ATTENTION : réglé en i2c, mettre un port virtuel
 
 
-############  GETTING LOCALISATION  ##############
+def get_acceleration_data() :
+    return np.array(accelSensor.get_accel_data())
 
-# possible libs : filterpy, pykalman
-# https://filterpy.readthedocs.io/en/latest/kalman/KalmanFilter.html
+
+def get_gyro_data() :
+    return np.array(accelSensor.get_gyro_data())
+
+
+#  INTEGRATERS  #
+
+
+def integrate_acceleration(previous_speed_vec: np.array, previous_position_vec: np.array, dt: np.float):
+    accel_vec = get_acceleration_data()
+    speed = np.array(accel_vec * dt + previous_speed_vec)
+    position = np.array(speed * dt + previous_position_vec)
+    
+    # ajouter un sleep convenable, car lancé dans un thread externe ou subproc
+    time.sleep(dt - h)
+    return speed, position
+
+
+def integrate_angle(previous_angle: np.float, dt: np.float):
+    gyro_speed = get_gyro_data()
+    angle = np.array(gyro_speed * dt + previous_angle)
+    return angle
+
+
+def speed_from_motors():
+    ...
+
+
+def integration_thread():
+    ...  # doit être plutôt créé dans le main ?
